bcc438683a0aa22b81fe1ca09319b0dcb7259932
Marshnm <jildsmiles@yahoo.com>
2022-02-21 20:53:52 -0500

    using JWT to generate token in reg and login

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -34,7 +34,8 @@ const registerUser = asyncHandler(async(req,res) => {
         res.status(201).json({
             _id:user.id,
             name:user.name,
-            email:user.email
+            email:user.email,
+            token: generateToken(user._id)
         })
     }else{
         res.status(400)
@@ -53,6 +54,7 @@ const loginUser = asyncHandler(async(req,res) => {
         _id:user.id,
         name:user.name,
         email:user.email,
+        token: generateToken(user._id)
     })
 }else{
     res.status(400)
@@ -66,6 +68,10 @@ const loginUser = asyncHandler(async(req,res) => {
 const getMe = asyncHandler(async(req,res) => {
     res.json({message:'Get my data'})
 })
+
+const generateToken = (id) =>{
+    return jwt.sign({id},process.env.JWT_SECRET,{expiresIn:'30d'})
+}
 
 
 
