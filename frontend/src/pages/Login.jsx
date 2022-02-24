b8bb3d9ce335d823b60aebfcccf158fd07a4d229
Joshua Holtsclaw <jildsmiles@yahoo.com>
2022-02-23 19:29:05 -0500

    finished login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,54 @@
 import React from 'react';
+import {useState,useEffect} from 'react'
+import {FaSignInAlt} from 'react-icons/fa'
+
 
 
 function Login() {
-    return(
-        <div>Login</div>
+
+    const [formData,setFormData] = useState({
+        email:'',
+        password:'',
+    })
+
+    const {email,password} = formData
+
+    const onChange = (e) => {
+        setFormData((prevState) =>({
+            ...prevState,
+            [e.target.name]:e.target.value
+        }))
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+    }
+
+     return(
+        <>
+            <section>
+                <h1><FaSignInAlt/> Login</h1>
+                <p>Login and start saving movies</p>
+            </section>
+            <section className="form">
+            
+                <div className="form-group">
+                    <form>
+                        <input type="email" ClassName="form-control" id="email" name="email" value = {email} placeholder="Enter your email" onChange={onChange}></input>
+                    </form>
+                </div>
+
+                <div className="form-group">
+                    <form>
+                        <input type="password" ClassName="form-control" id="password" name="password" value = {password} placeholder="Enter your password" onChange={onChange}></input>
+                    </form>
+                </div>
+                
+                <div className="form-group">
+                    <button className="btn btn-block">Submit</button>
+                </div>
+            </section>
+        </>
     )
 }
 
